01a63efea29a4b1a4333f9e942931e4cd276de67
Efrain Perez <efrain.abperez23@gmail.com>
2018-09-23 20:48:32 -0500

    added styles

--- src/app/components/rent-profile/rent-profile.component.ts
+++ src/app/components/rent-profile/rent-profile.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { RentProfile, RentResult } from './model/rent-profile';
 import { environment } from '../../../environments/environment';
 import * as _ from 'lodash';
+import { TooltipPosition, MatDialogRef, MatDialog } from '@angular/material';
+import { RentModalComponent } from '../rent-map/marker/rent-modal/rent-modal.component';
 
 @Component({
   selector: 'app-rent-profile',
@@ -14,10 +16,11 @@ export class RentProfileComponent implements OnInit {
 
   public rentResult: RentResult;
   public googleApiPhoto = `${environment.googleMapApi}/place/photo?maxwidth=2600&`;
+  public toolTip: TooltipPosition = 'above';
 
-  public constructor(private route: ActivatedRoute, private rentProfileService: RentProfileService) { }
+  public constructor(private route: ActivatedRoute, private rentProfileService: RentProfileService, private dialog: MatDialog) { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
     this.route.params.subscribe((params: Params): void => {
       if (params && params.id) {
         this.rentProfileService.getPlaceDetail(params.id).subscribe((rentProfile: RentProfile): void => {
@@ -26,11 +29,22 @@ export class RentProfileComponent implements OnInit {
             if (rentProfile.result.photos) {
               rentProfile.result.photos = _.orderBy(rentProfile.result.photos, ['width'], ['desc']);
               this.googleApiPhoto += `photoreference=${rentProfile.result.photos[0].photo_reference}&key=${environment.googleMapKey}`;
-              console.log(rentProfile);
             }
           }
         });
       }
+    });
+  }
+
+  public rentIt(): void {
+    const dialogRef: MatDialogRef<RentModalComponent, any> = this.dialog.open(RentModalComponent, {
+      width: '500px',
+      data: this.rentResult
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(this.rentResult);
     });
   }
 
