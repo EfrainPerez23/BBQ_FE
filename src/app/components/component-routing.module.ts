5aff5f5b230f56a2b5eee7b3eecaff0bb2b2d8a7
Efrain Perez <efrain.abperez23@gmail.com>
2018-09-23 22:06:32 -0500

    added added my rents views

--- src/app/components/component-routing.module.ts
+++ src/app/components/component-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RentMapComponent } from './rent-map/rent-map.component';
 import { RentProfileComponent } from './rent-profile/rent-profile.component';
+import { MyRentsComponent } from './my-rents/my-rents.component';
 
 const routes: Routes = [
   { path: '', component: RentMapComponent },
-  { path: 'bbq/:id', component: RentProfileComponent }
+  { path: 'bbq/:id', component: RentProfileComponent },
+  { path: 'rents', component: MyRentsComponent }
 ];
 
 @NgModule({
